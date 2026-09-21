50fa24570039e7c40e8fa1add7fe08c9371b0b26
Loctarogar <Morilon@protonmail.com>
2026-09-21 20:41:01 +0200

    timer added

--- app/src/main/java/com/example/godzinowka/MainActivity.kt
+++ app/src/main/java/com/example/godzinowka/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.godzinowka.ui.theme.GodzinowkaTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,12 +41,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var czyPracuje by remember { mutableStateOf(false) }
+    var sekundy by remember { mutableStateOf(0) }
+
+    LaunchedEffect(czyPracuje) {
+        while (czyPracuje) {
+            delay(1000L)
+            sekundy++
+        }
+    }
     Column(modifier = modifier) {
         Text(text = name)
         Text(text = if(czyPracuje) "Status: Praca w toku" else "Status: Zatrzymano" )
+        Text(text = "Czas pracy: ${sekundy}")
         Button(onClick = {czyPracuje = !czyPracuje}) {
             Text(text = if (czyPracuje) "Stop" else "Start")
-    }
+        }
    }
 }
 
