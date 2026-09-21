0abb5c3120f50c00fadafb097b11a7ea92e6602e
Loctarogar <Morilon@protonmail.com>
2026-09-21 21:12:07 +0200

    space between buttons, alignment

--- app/src/main/java/com/example/godzinowka/MainActivity.kt
+++ app/src/main/java/com/example/godzinowka/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,8 +20,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.godzinowka.ui.theme.GodzinowkaTheme
 import kotlinx.coroutines.delay
 
@@ -52,7 +57,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 
     var sformatowanyCzas = timer(sekundy)
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
         Text(text = name)
         Text(text = if(czyPracuje) "Status: Praca w toku" else "Status: Zatrzymano" )
         Text(text = "Czas pracy: $sformatowanyCzas")
@@ -60,7 +70,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Button(onClick = {czyPracuje = !czyPracuje}) {
                 Text(text = if (czyPracuje) "Stop" else "Start")
             }
-
+            Spacer(modifier = Modifier.width(16.dp))
             Button(onClick = {
                 czyPracuje = false
                 sekundy = 0
@@ -68,7 +78,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Reset")
             }
         }
-
    }
 }
 
