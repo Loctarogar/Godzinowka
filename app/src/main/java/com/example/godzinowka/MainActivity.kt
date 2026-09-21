d60d0721c59af3a51aea67428fb56f130befa90f
Loctarogar <Morilon@protonmail.com>
2026-09-21 22:01:56 +0200

    font change

--- app/src/main/java/com/example/godzinowka/MainActivity.kt
+++ app/src/main/java/com/example/godzinowka/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -22,8 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.godzinowka.ui.theme.GodzinowkaTheme
 import kotlinx.coroutines.delay
 
@@ -56,16 +60,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }
     }
 
-    var sformatowanyCzas = timer(sekundy)
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Text(text = name)
-        Text(text = if(czyPracuje) "Status: Praca w toku" else "Status: Zatrzymano" )
-        Text(text = "Czas pracy: $sformatowanyCzas")
+        Text(
+            text = name, fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = if(czyPracuje) "Status: Praca w toku" else "Status: Zatrzymano",
+            fontWeight = FontWeight.Bold
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = timer(sekundy),
+            fontSize = 48.sp,
+            fontWeight =  FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(24.dp))
         Row {
             Button(onClick = {czyPracuje = !czyPracuje}) {
                 Text(text = if (czyPracuje) "Stop" else "Start")
